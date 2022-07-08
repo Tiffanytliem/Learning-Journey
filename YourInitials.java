33d4ebfb4304347219043a426434aa273143558e
Tiffanytliem <108093252+Tiffanytliem@users.noreply.github.com>
2022-07-07 20:35:17 -0700

    Add files via upload

---
+++ YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        // print initials here.
+        System.out.println("TTTTT  L    ");
+        System.out.println("  T    L    ");
+        System.out.println("  T    L    ");
+        System.out.println("  T    L    ");
+        System.out.println("  T    L    ");
+        System.out.println("  T    L    ");
+        System.out.println("  T    LLLLL");
+    }
+}
